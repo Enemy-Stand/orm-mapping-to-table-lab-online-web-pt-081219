1b910518f63dab0be110a9635d65e348b779ee44
Joseph Sesti <sestijoey63@gmail.com>
2019-10-24 19:12:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
     sql = <<- SQL
     CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY,
     name TEXT, grade INTEGER) SQL
-    DB[:conn]
+    DB[:conn].execute
   end
 
   # Remember, you can access your database connection anywhere in this class

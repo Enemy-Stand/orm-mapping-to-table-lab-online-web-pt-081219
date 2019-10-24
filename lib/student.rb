49e28ebd1ae906b11a425ff091f9907d9c6f7675
Joseph Sesti <sestijoey63@gmail.com>
2019-10-24 19:13:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,7 @@ class Student
     CREATE TABLE IF NOT EXISTS students
       (id INTEGER PRIMARY KEY,
       name TEXT,
-      grade INTEGER) SQL
+      grade TEXT) SQL
       DB[:conn].execute(sql)
     end
   end
